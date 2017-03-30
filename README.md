cea1c44e6752f87643cce794df7c9f22e0b6e462
root <you@example.com>
2017-03-30 20:18:51 +0200

    add changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-### Project Hackathon
+# Project Hackathon
+
+VueJs + Vue Material + Roboto Font + Icons Material 
+
+![enter image description here](http://projectsquare.io/wp-content/uploads/2016/06/logo-vuejs.png)
+
+![enter image description here](https://camo.githubusercontent.com/4486674a0ccf230066fa4bfd5fe4624ab1ef85ce/68747470733a2f2f7675656d6174657269616c2e6769746875622e696f2f6173736574732f6c6f676f2d7675652d6d6174657269616c2d64656661756c742e706e67)
--- index.js
+++ index.js
@@ -9,7 +9,9 @@ Vue.material.registerTheme('default', {
   accent: 'red',
   warn: 'red',
   background: 'white'
-})
+});
+
+
 
 
 let app = new Vue({
@@ -23,16 +25,12 @@ let app = new Vue({
 			Materialize.toast('Ajouté à vos favoris!', 4000) // 4000 is the duration of the toast
 		},
 		searching: function(){
-			app.movies = [];
 			if(this.search.length >= 3){
-					$.getJSON(`http://www.omdbapi.com/?plot=full&r=json&t=${this.search}`, function(data) {
-						
+					$.getJSON(`http://www.omdbapi.com/?plot=full&r=json&s=${this.search}`, function(data) {
 						if(data.Response !== "False"){
-							app.movies.push(data);
-						
-					}
+							app.movies = data.Search;
+						}
 					});
-
 			}
 			
 
@@ -40,9 +38,3 @@ let app = new Vue({
 	}
 });
 
-/**
- * Instance Jquery 
- */
-$.getJSON("http://www.omdbapi.com/?t=pou", function( data ) {
-	app.movies.push(data);
-});
