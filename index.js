022ab785da13c258a9944d2b6ff66882f82e99f6
root <you@example.com>
2017-03-31 17:30:58 +0200

    add changes

--- index.js
+++ index.js
@@ -14,33 +14,68 @@ Vue.material.registerTheme('default', {
 
 
 
+// 1. Define route components.
+// These can be imported from other files
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// Vue.extend(), or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for routes: routes
+})
+
+let data = { nb: 0 }
+
+Vue.component('my-main', {
+	  template: '<button v-on:click="counter += 1">Le nombre est {{ counter }}</button>',
+		data: {
+			counter: data
+		}
+});
+
+
 let app = new Vue({
+  router,
 	el: '#app',
 	data: {
 		search: '',
 		selected: false,
+		selectedMovie: null,
+		favoritesMovie: [],
 		movies: []
 	},
 	methods: {
-		onOpen() {
-
+		onClose(state) {
+			if(state == 'ok'){
+				let position = this.movies.indexOf(this.selectedMovie);
+				this.movies.splice(position, 1);
+			}
 		},
-		onClose() {
-
-		},
-		openDialog(ref) {
-			console.log(ref)
+		openDialog(ref, movie) {
+			this.selectedMovie = movie; 
 			this.$refs[ref].open();
 		},
 		closeDialog(ref) {
-			this.$refs[ref].close();
+			this.$refs[ref].close(movie);
 		},
 
 		toggleLeftSidenav() {
 			this.$refs.leftSidenav.toggle();
 		},
+
 		more: function () {
-			// random letter
 			let emptyString = "";
 			let alphabet = "abcdefghijklmnopqrstuvwxyz";
 			emptyString += alphabet[Math.floor(Math.random() * alphabet.length)];
@@ -55,9 +90,16 @@ let app = new Vue({
 
 		love: function (movie) {
 			this.$refs.snackbar.open();
-			let position = this.movies.indexOf(movie);
-			this.movies[position].like = !this.movies[position].like;
+			let position = this.favoritesMovie.indexOf(movie);
+			console.log(position)
+			if(position > -1){
+				this.favoritesMovie.splice(position, 1);
+			}else{
+				this.favoritesMovie.push(movie);
+			}
+
 		},
+
 		searching: function () {
 			if (this.search.length >= 3) {
 				$.getJSON(`http://www.omdbapi.com/?plot=full&r=json&s=${this.search}`, function (data) {
@@ -66,13 +108,8 @@ let app = new Vue({
 					}
 				});
 			}
-
-
 		},
-		remove: function (movie) {
-			let position = this.movies.indexOf(movie);
-			this.movies.splice(position, 1);
-		}
+
 	}
 });
 
